e4227ae7198c4d3c7170073a4d76482b74885d9f
1emonislam <emonislamweb@gmail.com>
2022-07-18 21:32:56 +0600

    boo

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import 'dotenv/config';
 import 'module-alias/register';
 import validateEnv from '@/utils/validateEnv';
 import App from './app';
+import PostController from './resources/post/post.controller';
 validateEnv();
-const app = new App([], Number(process.env.PORT));
+const app = new App([new PostController()], Number(process.env.PORT));
 app.listen()
